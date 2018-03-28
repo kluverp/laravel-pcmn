ef228802b5b69875224993e4c7707ad5b1f7ef0e
peter <peter@samson-it.nl>
2018-03-28 19:44:14 +0200

    Only apply rounded corners on last sub-item of navigation

--- src/Views/_components/sidebar.blade.php
+++ src/Views/_components/sidebar.blade.php
@@ -6,14 +6,14 @@
             @if(!empty($item['label']))
                 {{ $item['label'] }}
             @else
-                &lt; ! missing label &gt;
+                &lt;missing label&gt;
             @endif
         </a>
         @if(!empty($item['tables']))
             <div class="list-group">
                 @foreach($item['tables'] as $table)
                     <a href="@if(!empty($table['url'])){{ $table['url'] }}@else#@endif"
-                       class="list-group-item d-flex justify-content-between align-items-center list-group-item-action">
+                       class="list-group-item d-flex justify-content-between align-items-center list-group-item-action @if($loop->last)last @endif">
                         @if(!empty($table['label']))
                             {{ $table['label'] }}
                         @endif
@@ -26,27 +26,4 @@
         @endif
     @endforeach
 
-    {{--
-    <a href="#" class="list-group-item d-flex justify-content-between align-items-center list-group-item-action list-group-item-secondary">
-        Cras justo odio
-    </a>
-    <div class="list-group">
-        <a href="#" class="list-group-item d-flex justify-content-between align-items-center list-group-item-action text-primary">
-            Dapibus ac facilisis in
-            <span class="badge badge-primary badge-pill">14</span>
-        </a>
-        <a href="#" class="list-group-item d-flex justify-content-between align-items-center list-group-item-action">
-            Dapibus ac facilisis in
-            <span class="badge badge-secondary badge-pill">14</span>
-        </a>
-        <a href="#" class="list-group-item d-flex justify-content-between align-items-center list-group-item-action">
-            Dapibus ac facilisis in
-            <span class="badge badge-secondary badge-pill">14</span>
-        </a>
-    </div>
-
-    <a href="#" class="list-group-item d-flex justify-content-between align-items-center list-group-item-action">Dapibus ac facilisis in</a>
-    <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-    <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-    --}}
 </div>
